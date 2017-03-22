a273a18cb4a372647a558d632f0032c7c99d1225
02100 <yiguozhi@qccr.com>
2017-03-22 19:25:26 +0800

    update

---
+++ example/index.js
@@ -0,0 +1,21 @@
+/*
+* @Author: wj77998
+* @Date:   2017-03-02 19:09:56
+* @Email:  wj77998@qq.com
+* @Last Modified by:   wj77998
+* @Last Modified time: 2017-03-02 19:37:27
+*/
+     
+import React from 'react'
+
+import { render } from 'react-dom';
+
+import List from '../src/index'
+
+import "../assets/index.less"
+
+render(
+  <List 
+    
+  />
+, document.getElementById('app'));
